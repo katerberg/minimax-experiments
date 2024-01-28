351551289d6da451605a6f9162b4ed8a01fc4bda
Mark Katerberg <katerberg@fastmail.fm>
2024-01-28 14:44:13 -0600

    Starting point for webpack and typescript

---
+++ README.md
@@ -0,0 +1,21 @@
+# Minimax Experiments
+
+A playground for minimax performance experiments in anticipation of the 7drl
+
+## Installation
+
+```js
+nvm use
+npm install
+```
+
+## Running
+
+```js
+npm start
+```
+
+## Deploying
+
+Deploys automagically with Github actions
+
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import './index.scss';
+
+const gameWidth = 800;
+const gameHeight = 375;
+const gameRatio = gameWidth / gameHeight;
+function resize(): void {
+  const gameNode = document.getElementById('game');
+  gameNode?.classList.remove('vertical-constraint', 'horizontal-constraint');
+  const windowRatio = window.innerWidth / (window.innerHeight - 120);
+  if (windowRatio < 0 || windowRatio > 10) {
+    return;
+  }
+  if (gameRatio < windowRatio) {
+    gameNode?.classList.add('vertical-constraint');
+  } else {
+    gameNode?.classList.add('horizontal-constraint');
+  }
+}
+
+resize();
+addEventListener('resize', () => {
+  resize();
+});
