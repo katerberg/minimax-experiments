092f92921bd28366211e2df75e967fe13322ab70
Mark Katerberg <katerberg@fastmail.fm>
2024-02-17 21:14:13 -0600

    Simplifies logic, 3x4 is 3949ms

--- src/minimax.ts
+++ src/minimax.ts
@@ -8,78 +8,42 @@ export function getBestMove(
   isMaximizing?: boolean,
   depth = 0,
 ): {bestScore: number; bestMove: NumberCoordinates} {
-  const maximizing = isMaximizing ? isMaximizing : state.currentPlayer === 'x';
+  const maximizing = isMaximizing !== undefined ? isMaximizing : state.currentPlayer === 'x';
   if (depth > state.maxDepth) {
     return {bestScore: 0, bestMove: {x: 0, y: 0}};
   }
-  if (maximizing) {
-    //Loop through all empty cells
-    const availableMoves = getAvailableMoves(state);
-    //Initialize best to the lowest possible value and have a default move
-    let [bestMove] = availableMoves;
-    let bestScore = -1_000_000;
-    for (let i = 0; i < availableMoves.length; i++) {
-      const move = availableMoves[i];
-
-      //Initialize a new board with a copy of our current state
-      const child: State = {
-        ...state,
-        currentPlayer: 'o',
-        selections: {...state.selections},
-      };
-      //Create a child node by inserting the maximizing symbol x into the current empty cell
-      child.selections[numberCoordsToCoords(move)] = 'x';
-      const terminalState = checkTerminal(child);
-      if (terminalState.isTerminal) {
-        if (terminalState.isWinner) {
-          bestScore = 1_000_000 - depth;
-          bestMove = move;
-          break;
-        }
-        if (terminalState.isCat && 0 > bestScore) {
-          bestScore = 0;
-          bestMove = move;
-        }
-      } else {
-        const nodeValue = getBestMove(child, false, depth + 1);
-        if (nodeValue.bestScore > bestScore) {
-          ({bestScore} = nodeValue);
-          bestMove = move;
-        }
-      }
-    }
-    return {bestScore, bestMove};
-  }
-  //Loop through all empty cells
   const availableMoves = getAvailableMoves(state);
-  //Initialize best to the highest possible value and have a default move
   let [bestMove] = availableMoves;
-  let bestScore = 1_000_000;
+  //Initialize best to the worst possible value and have a default move
+  const WORST_POSSIBLE_SCORE = maximizing ? -1_000_000 : 1_000_000;
+  const DEPTH_MULTIPLIER = maximizing ? 1 : -1;
+  let bestScore = WORST_POSSIBLE_SCORE;
+
   for (let i = 0; i < availableMoves.length; i++) {
     const move = availableMoves[i];
-
     //Initialize a new board with a copy of our current state
     const child: State = {
       ...state,
-      currentPlayer: 'x',
+      currentPlayer: maximizing ? 'o' : 'x',
       selections: {...state.selections},
     };
-    //Create a child node by inserting the minimizing symbol o into the current empty cell
-    child.selections[numberCoordsToCoords(move)] = 'o';
+    child.selections[numberCoordsToCoords(move)] = maximizing ? 'x' : 'o';
     const terminalState = checkTerminal(child);
     if (terminalState.isTerminal) {
       if (terminalState.isWinner) {
-        bestScore = -1_000_000 + depth;
+        bestScore = WORST_POSSIBLE_SCORE * -1 - depth * DEPTH_MULTIPLIER;
         bestMove = move;
         break;
       }
-      if (terminalState.isCat && 0 < bestScore) {
-        bestScore = 0;
-        bestMove = move;
+      if (terminalState.isCat) {
+        if ((maximizing && 0 > bestScore) || (!maximizing && 0 < bestScore)) {
+          bestScore = 0;
+          bestMove = move;
+        }
       }
     } else {
-      const nodeValue = getBestMove(child, true, depth + 1);
-      if (nodeValue.bestScore < bestScore) {
+      const nodeValue = getBestMove(child, !maximizing, depth + 1);
+      if ((maximizing && nodeValue.bestScore > bestScore) || (!maximizing && nodeValue.bestScore < bestScore)) {
         ({bestScore} = nodeValue);
         bestMove = move;
       }
